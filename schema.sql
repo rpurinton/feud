00d3cb4a37cde45e9715ec107a0ee5e8c452128e
Russell Purinton <russell.purinton@gmail.com>
2025-06-07 13:55:46 +0000

    Pushed 2025-06-07 13:55:46

--- schema.sql
+++ schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE `answers` (
   KEY `idx_answers_question_id` (`question_id`),
   CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=8254 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 DROP TABLE IF EXISTS `game_answers`;
 CREATE TABLE `game_answers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,17 +21,20 @@ CREATE TABLE `game_answers` (
   KEY `idx_game_answers_player_id` (`player_id`),
   KEY `idx_game_answers_game_id` (`game_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 DROP TABLE IF EXISTS `games`;
 CREATE TABLE `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `channel_id` bigint(20) NOT NULL,
   `question_id` int(11) NOT NULL,
   `timeout_at` bigint(20) NOT NULL,
+  `playable` tinyint(1) NOT NULL DEFAULT 0,
   `completed` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `channel_id` (`channel_id`,`completed`),
   KEY `idx_games_channel_id` (`channel_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 DROP TABLE IF EXISTS `questions`;
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
