1f526b93cb439e93cfdd7446d8c0e20288e3ed2d
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-10 12:23:22 -0400

    /feud Command

--- src/events/messageCreate.mjs
+++ src/events/messageCreate.mjs
@@ -3,9 +3,6 @@ import { getMsg } from '../locales.mjs';
 
 // Event handler for messageCreate
 export default async function (message) {
-    log.debug('messageCreate', { message });
+    //log.debug('messageCreate', { message });
     if (message.author.bot) return;
-    if (message.content === '!ping') {
-        message.reply('Pong!');
-    }
 }
